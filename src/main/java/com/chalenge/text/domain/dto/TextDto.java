0030a3cd88a1393a1e73155116d4643385d25abd
Kirad20 <cesard20@hotmail.com>
2022-03-15 10:04:44 -0600

    update TextDTO

--- src/main/java/com/chalenge/text/domain/dto/TextDto.java
+++ src/main/java/com/chalenge/text/domain/dto/TextDto.java
@@ -6,13 +6,17 @@ import java.util.Map;
 import javax.validation.constraints.Min;  
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class TextDto {
   
